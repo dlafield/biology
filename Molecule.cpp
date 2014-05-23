7c7dcc2aa190aae69d855b4fdd494a792c5f5358
dlafield <wlafie2@lsu.edu>
2014-05-22 20:58:57 -0500

    commit 201405222057

---
+++ Molecule.cpp
@@ -0,0 +1,183 @@
+#include <cstring>
+#include <vector>
+
+#include "Molecule.h"
+
+#include "EdgeAggregator.h"
+
+
+void Molecule::SetValues(char name_p[], int numAtoms_p, int numBonds_p, int numSubStructs_p, int numFeatures_p, int numSets_p, char moleculeType_p[], char chargeType_p[], char statusBit_p[], char comments_p[])
+{
+	/* set literal values */
+	this->name = name_p;
+	this->numAtoms = numAtoms_p;
+	this->numBonds = numBonds_p;
+	this->numSubStructs = numSubStructs_p;
+	this->numFeatures = numFeatures_p;
+	this->numSets = numSets_p;
+	this->comments, comments_p;
+
+	/* Because the moleculeType is a string with only 5 possible values, I'll store it as an integer */
+	this->typeOfMolecule = -1; /* default value, if the moleculeType is unassigned */
+	if(strcmp(moleculeType_p, "SMALL") == 0)          this->typeOfMolecule = SMALL;
+	if(strcmp(moleculeType_p, "BIOPOLYMER") == 0)     this->typeOfMolecule = BIOPOLYMER;
+	if(strcmp(moleculeType_p, "PROTEIN") == 0)        this->typeOfMolecule = PROTEIN;
+	if(strcmp(moleculeType_p, "NUCLEIC_ACID") == 0)   this->typeOfMolecule = NUCLEIC_ACID;
+	if(strcmp(moleculeType_p, "SACCHARIDE") == 0)     this->typeOfMolecule = SACCHARIDE;
+
+	/* Because the chargeType is a string with only 12 possible values, I'll store it as an integer */
+	this->chargeType = -1; /* default value, if the chargeType is unassigned */
+	if(strcmp(chargeType_p, "NO_CHARGES") == 0)       this->chargeType = NO_CHARGES;
+	if(strcmp(chargeType_p, "DEL_RE") == 0)           this->chargeType = DEL_RE;
+	if(strcmp(chargeType_p, "GASTEIGER") == 0)        this->chargeType = GASTEIGER;
+	if(strcmp(chargeType_p, "GAST_HUCK") == 0)        this->chargeType = GAST_HUCK;
+	if(strcmp(chargeType_p, "HUCKEL") == 0)           this->chargeType = HUCKEL;
+	if(strcmp(chargeType_p, "PULLMAN") == 0)          this->chargeType = PULLMAN;
+	if(strcmp(chargeType_p, "GAUSS80_CHARGES") == 0)  this->chargeType = GAUSS80_CHARGES;
+	if(strcmp(chargeType_p, "AMPAC_CHARGES") == 0)    this->chargeType = AMPAC_CHARGES;
+	if(strcmp(chargeType_p, "MULLIKEN_CHARGES") == 0) this->chargeType = MULLIKEN_CHARGES;
+	if(strcmp(chargeType_p, "DICT_CHARGES") == 0)     this->chargeType = DICT_CHARGES;
+	if(strcmp(chargeType_p, "MMFF94_CHARGES") == 0)   this->chargeType = MMFF94_CHARGES;
+	if(strcmp(chargeType_p, "USER_CHARGES") == 0)     this->chargeType = USER_CHARGES;
+
+	/* Because the statusBits is a string with only 6 possible values, I'll store it as an integer */
+	this->statusBits = -1; /* default value, if the statusBits is unassigned */
+	if(strcmp(statusBit_p, "system") == 0 || strcmp(statusBit_p, "System") == 0 || strcmp(statusBit_p, "SYSTEM") == 0)
+		this->statusBits = SYSTEM;
+	if(strcmp(statusBit_p, "invalid_charges") == 0 || strcmp(statusBit_p, "Invalid_charges") == 0 || strcmp(statusBit_p, "INVALID_CHARGES") == 0)
+		this->statusBits = INVALID_CHARGES;
+	if(strcmp(statusBit_p, "analyzed") == 0 || strcmp(statusBit_p, "Analyzed") == 0 || strcmp(statusBit_p, "ANALYZED") == 0)
+		this->statusBits = ANALYZED;
+	if(strcmp(statusBit_p, "substituted") == 0 || strcmp(statusBit_p, "Substituted") == 0 || strcmp(statusBit_p, "SUBSTITUTED") == 0)
+		this->statusBits = SUBSTITUTED;
+	if(strcmp(statusBit_p, "altered") == 0 || strcmp(statusBit_p, "Altered") == 0 || strcmp(statusBit_p, "ALTERED") == 0)
+		this->statusBits = ALTERED;
+	if(strcmp(statusBit_p, "ref_angle") == 0 || strcmp(statusBit_p, "Ref_angle") == 0 || strcmp(statusBit_p, "REF_ANGLE") == 0)
+		this->statusBits = REF_ANGLE;
+
+}
+
+/****************************************************************************************/
+
+int Molecule::Parse(char moleculeName[], char moleculeStats[], char typeOfMolecule[], char moleculeCharge[], char moleculeStatusBits[], char comments[])
+{
+	/*
+		Input: line from file, and open file that is currently in a molecule section
+		Output: molecule, and the file in the input has advanced to the next section
+	*/
+
+	int numAtoms_p; 
+	int numBonds_p; 
+	int numSubStructs_p; 
+	int numFeatures_p; 
+	int numSets_p;
+	int index = 0;
+
+	/* get number of atoms from stats */
+	while(moleculeStats[index] == ' ' || moleculeStats[index] == '\t')	//get past white space
+		index++;
+	numAtoms_p = moleculeStats[index] - '0';
+	index++;
+	while(moleculeStats[index] != ' ' && moleculeStats[index] != '\t' && moleculeStats[index] != '\n' && moleculeStats[index] != '\0')
+	{ /* parse number */
+		numAtoms_p *= 10;
+		numAtoms_p += moleculeStats[index] - '0';
+		index++;
+	} /* parse number */
+
+	while(moleculeStats[index] == ' ' || moleculeStats[index] == '\t')	//get past white space but not newline
+		index++;
+	
+	if(moleculeStats[index] != '\n' && moleculeStats[index] != '\0')
+	{ /* if not end of line */
+
+		/* get number of bonds */
+		numBonds_p = moleculeStats[index] - '0';
+		index++;
+		while(moleculeStats[index] != ' ' && moleculeStats[index] != '\t' && moleculeStats[index] != '\n' && moleculeStats[index] != '\0')
+		{ /* parse number */
+			numBonds_p *= 10;
+			numBonds_p += moleculeStats[index] - '0';
+			index++;
+		} /* parse number */
+
+	} /* if not end of line */
+
+	while(moleculeStats[index] == ' ' || moleculeStats[index] == '\t')	//get past white space but not newline
+		index++;
+
+	if(moleculeStats[index] != '\n' && moleculeStats[index] != '\0')
+	{ /* if not end of line */
+
+		numSubStructs_p = moleculeStats[index] - '0';
+		index++;
+		while(moleculeStats[index] != ' ' && moleculeStats[index] != '\t' && moleculeStats[index] != '\n' && moleculeStats[index] != '\0')
+		{ /* get num_subst */
+			numSubStructs_p *= 10;
+			numSubStructs_p += moleculeStats[index] - '0';
+			index++;	
+		} /* parse number */
+	} /* if not end of line */
+
+	while(moleculeStats[index] == ' ' || moleculeStats[index] == '\t')	//get past white space but not newline
+		index++;
+
+	if(moleculeStats[index] != '\n' && moleculeStats[index] != '\0')
+	{ /* if not end of line */
+		
+		numFeatures_p = moleculeStats[index] - '0';
+		index++;
+		while(moleculeStats[index] != ' ' && moleculeStats[index] != '\t' && moleculeStats[index] != '\n' && moleculeStats[index] != '\0')
+		{ /* get num_feat */
+			numFeatures_p *= 10;
+			numFeatures_p += moleculeStats[index] - '0';
+			index++;
+		} /* parse number */
+	} /* if not end of line */
+
+	while(moleculeStats[index] == ' ' || moleculeStats[index] == '\t')	//get past white space but not newline
+		index++;
+
+	if(moleculeStats[index] != '\n' && moleculeStats[index] != '\0')
+	{ /* if not end of line */
+		
+		numSets_p = moleculeStats[index] - '0';
+		index++;
+		while(moleculeStats[index] != ' ' && moleculeStats[index] != '\t' && moleculeStats[index] != '\n' && moleculeStats[index] != '\0')
+		{ /* get num_set */
+			numSets_p *= 10;
+			numSets_p += moleculeStats[index] - '0';
+			index++;
+		} /* parse number */
+	} /* if not end of line */
+
+	/* set molecule values */
+	SetValues(moleculeName, numAtoms_p, numBonds_p, numSubStructs_p, numFeatures_p, numSets_p, typeOfMolecule, moleculeCharge, moleculeStatusBits, comments);
+
+} /* parseMolecule */
+
+/****************************************************************************************/
+
+std::vector<EdgeAggregator> Molecule::Compose(const Molecule& thatMolecule)
+{
+    std::vector<EdgeAggregator> newMolecules;
+    
+    return newMolecules;
+}
+
+bool Molecule::Equals(const Molecule&) const
+{
+    return false;
+}
+
+std::string Molecule::toString() const
+{
+    return this->name;
+}
+
+std::ostream& operator<< (std::ostream& os, Molecule& mol)
+{
+    os << mol.toString();
+    
+    return os;
+}
---
+++ Molecule.h
@@ -0,0 +1,78 @@
+#ifndef _MOLECULE_GUARD
+#define _MOLECULE_GUARD 1
+
+#include <string>
+#include <vector>
+
+class EdgeAggregator;
+
+typedef enum
+{
+	SMALL=1,
+   BIOPOLYMER=2,
+   PROTEIN=3,
+   NUCLEIC_ACID=4,
+   SACCHARIDE=5
+} eTypeOfMoleculeT;
+
+typedef enum
+{
+	NO_CHARGES=1,
+   DEL_RE=2,
+   GASTEIGER=3,
+   GAST_HUCK=4,
+   HUCKEL=5,
+   PULLMAN=6,
+   GAUSS80_CHARGES=7,
+   AMPAC_CHARGES=8,
+   MULLIKEN_CHARGES=9,
+   DICT_CHARGES=10,
+   MMFF94_CHARGES=11,
+   USER_CHARGES=12
+} eTypeOfChargeT;
+
+typedef enum
+{
+	SYSTEM=1,
+   INVALID_CHARGES=2,
+   ANALYZED=3,
+   SUBSTITUTED=4,
+   ALTERED=5,
+   REF_ANGLE=6
+} eStatusBitT;
+
+class Molecule
+{
+  public:
+    int id;
+    int numAtoms;
+    int numBonds;
+    int numSubStructs;
+    int numFeatures;
+    int numSets;
+    eTypeOfMoleculeT typeOfMolecule;
+    eTypeOfChargeT chargeType;
+    eStatusBitT statusBits;
+	 std::string name;
+    std::string comments;
+
+    Molecule() {}
+    ~Molecule() {}
+
+    void setGraphID(int id) { graphID = id; }
+    int getGraphID() { return graphID; }
+    virtual std::vector<EdgeAggregator> Compose(const Molecule&);
+    virtual bool Equals(const Molecule&) const;
+
+    void SetValues(char name_p[], int numAtoms_p, int numBonds_p, int numSubStructs_p, int numFeatures_p, int numSets_p, char moleculeType_p[], char chargeType_p[], char statusBit_p[], char comments_p[]);
+    int Parse(char moleculeName[], char moleculeStats[], char typeOfMolecule[], char moleculeCharge[], char moleculeStatusBits[], char comments[]);
+
+    std::string toString() const;
+    friend std::ostream& operator<< (std::ostream& os, Molecule& mol);
+	operator int() {return id;}
+
+  private:
+    int graphID;
+};
+
+#endif
